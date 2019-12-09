825ef88cbf2e5d7b076f3f09313457b5ce8f89e7
장세훈 <jangsehun92@gmail.com>
2019-12-09 19:27:02 +0900

    window 환경 테스트겸 커밋

--- src/main/java/jsh/project/board/dao/BoardDao.java
+++ src/main/java/jsh/project/board/dao/BoardDao.java
@@ -4,5 +4,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BoardDao {
+	
+	private SqlSession sqlSession;
+	
+	public BoardDao(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
 
 }
